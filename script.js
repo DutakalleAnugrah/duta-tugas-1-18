d400c8cef5831b3bdace64ad6ca31a7b69e9bbde
DutakalleAnugrah <sitichotimahtun@gmail.com>
2026-01-13 13:57:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("mirai");
+
+let nama = "jamal"
+
+if (nama === "mirai"){
+    console.log("hai mirai cantik ");
+} else if(nama === "jamal"){
+    console.log("hai jamal jamet");
+} else {
+    console.log("kamu bukan siapa2 ");
+}
+
+switch(nama){
+    case "mirai":
+        console.log("switch case mirai");
+        break;
+    case "jamal":
+        console.log("switch case mirai")
+        break;
+    default:
+        console.log("switch case nobody")
+}
